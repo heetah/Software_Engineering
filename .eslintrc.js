d1db8b79a8cbd6bae2dc9021208be3db7fc1d4f9
heetah <heetah6716@gmail.com>
2025-10-14 20:35:21 +0800

    ?蔭 ESLint + Prettier ?舀憭?閮嚗TML?avaScript?SS?SON?ode.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,36 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['html', 'json'],
   rules: {
     indent: ['error', 2],
-    'linebreak-style': ['error', 'windows'],
+    'linebreak-style': 'off', // 關閉行結束符檢查，因為我們支援跨平台
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
     'no-unused-vars': 'warn',
     'no-console': 'off',
   },
-  ignorePatterns: ['*.html'],
+  ignorePatterns: [],
+  overrides: [
+    {
+      files: ['*.html'],
+      plugins: ['html'],
+      rules: {
+        indent: 'off',
+        quotes: 'off',
+        semi: 'off',
+        'no-console': 'off',
+      },
+    },
+    {
+      files: ['*.json'],
+      plugins: ['json'],
+      rules: {
+        indent: 'off',
+        quotes: 'off',
+        semi: 'off',
+        'no-console': 'off',
+      },
+    },
+  ],
 };
