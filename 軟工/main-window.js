13dad7c965de5a47d05b04440157f93656f52a4f
Yao <a0969811581@gmail.com>
2025-10-15 21:38:22 +0800

    chore: add skeleton files for main window (HTML/JS/SCSS)

---
+++ 軟工/main-window.js
@@ -0,0 +1,9 @@
+(function () {
+  "use strict";
+  document.addEventListener("DOMContentLoaded", function () {
+    const content = document.getElementById("content");
+    if (content) {
+      content.textContent = "Hello, world!";
+    }
+  });
+})();
